c207d10b46ed523fde2d288e0974f60b5833fe61
AutoBit <team@autobit.co>
2025-09-16 22:44:59 +0530

    bug solve

--- dsl/evaluator/tables.go
+++ dsl/evaluator/tables.go
@@ -31,8 +31,8 @@ func (e *Evaluator) EvalTableWithContext() error {
 		e.Plan.Pos = e.Plan.Pos - 1
 		return e.EvalTable()
 	}
-	for _, v := range e.ContextValues {
-		cntxQuery = strings.Replace(cntxQuery, "?", v, 1)
+	for i, v := range e.ContextValues {
+		cntxQuery = strings.Replace(cntxQuery, "$"+strconv.Itoa(i+1), v, 1)
 	}
 	lexer := parser.NewLexer(cntxQuery)
 	plan := parser.NewPlan(lexer, e.Plan.ProtocolPass)
